af627a323afd83e57bda671aacebc29c9da5d986
IkuakiR <linyuzhao3@gmail.com>
2023-11-08 11:07:24 +0900

    add practice

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -5,8 +5,8 @@ import { useState } from 'react'
 export default function Home() {
 
   const [state, setState] = useState(0)
-  const [value1, setValue1] = useState(0)
-  const [value2, setValue2] = useState(0)
+  const [value1, setValue1] = useState('')
+  const [value2, setValue2] = useState('')
 
   const Ans = () => {
     const num1 = Number(value1) || 0
@@ -19,7 +19,7 @@ export default function Home() {
       <div>
         <p>足し算アプリ</p>
         <p>{state}</p>
-        <Add value1='' value2='' />
+        <Add value1={value1} value2={value2} />
         <button onClick={Ans}>計算</button>
       </div>
     </>
